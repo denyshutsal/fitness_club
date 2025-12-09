41f48bc6a4942321d0303fa556d76ecd814bd9a8
Denys Hutsal <denyshutsal@gmail.com>
2025-12-09 10:37:39 +0100

    restored the ability to edit the admin profile

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,9 @@ Route::middleware(['auth', AdminMiddleware::class])->group(function () {
     // Users CRUD
     Route::resource('users', UserController::class);
 
-    Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
 });
 
 require __DIR__.'/auth.php';
